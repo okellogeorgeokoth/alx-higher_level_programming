bbbc45005140f9e3cdab4ebc1a6ec6048fea4a07
George Okello <okoth603@gmail.com>
2022-06-03 14:36:19 +0300

    My commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
-
-if __name__ == '__main__':
-    argv = sys.argv[1:]
-    arg_num = len(argv)
-    if arg_num != 3:
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    args = sys.argv
+    if len(args) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    if args[2] != '+' and args[2] != '-' and args[2] != '*' and args[2] != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    elif sys.argv[2] not in '+-*/':
-        print('Unknown operator. Available operators: +, -, *, and /')
-        exit(1)
-    else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        if sys.argv[2] is '+':
-            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-        elif sys.argv[2] is '-':
-            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-        elif sys.argv[2] is '*':
-            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
-        elif sys.argv[2] is "/":
-            print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+    a = int(args[1])
+    b = int(args[3])
+    if args[2] == '+':
+        print("{} {} {} = {}".format(a, args[2], b, add(a, b)))
+    elif args[2] == '-':
+        print("{} {} {} = {}".format(a, args[2], b, sub(a, b)))
+    elif args[2] == '*':
+        print("{} {} {} = {}".format(a, args[2], b, mul(a, b)))
+    elif args[2] == '/':
+        print("{} {} {} = {}".format(a, args[2], b, div(a, b)))
