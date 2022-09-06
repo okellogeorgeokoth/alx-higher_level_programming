fb188adcfd91e09d4c3013d0fb0257151a227427
George Okello <okoth603@gmail.com>
2022-09-06 16:01:13 +0300

    My commit

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// Class for rectangle with height and width parameters and various methods
+
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
@@ -7,20 +7,17 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
+
   print () {
     for (let i = 0; i < this.height; i++) {
-      for (let j = 0; j < this.width; j++) {
-        process.stdout.write('X');
-      }
-      process.stdout.write('\n');
+      console.log('X'.repeat(this.width));
     }
   }
 
   rotate () {
-    let temp = this.height;
-    this.height = this.width;
-    this.width = temp;
+    [this.width, this.height] = [this.height, this.width];
   }
+
   double () {
     this.width *= 2;
     this.height *= 2;
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
-// Reverses a list
 
 exports.esrever = function (list) {
-  let left = 0;
-  let right = list.length - 1;
-  while (left <= right) {
-    let temp = list[right];
-    list[right] = list[left];
-    list[left] = temp;
-    left++;
-    right--;
+  const output = [];
+
+  while (list.length) {
+    output.push(list.pop());
   }
-  return (list);
+
+  return output;
 };
