85d5e1f5f5d2422808cb7c2199d2e8a9f3213d55
GEORGE OKELLO <45455816+okellogeorgeokoth@users.noreply.github.com>
2022-09-06 15:53:50 +0300

    Update 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 // Class for rectangle with height and width parameters and various methods
-
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
@@ -8,7 +7,6 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
-
   print () {
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
@@ -23,7 +21,6 @@ module.exports = class Rectangle {
     this.height = this.width;
     this.width = temp;
   }
-
   double () {
     this.width *= 2;
     this.height *= 2;
